55f85426650214c84b5d9897701d161a2bd40005
gbabudoh <gbabudoh@gmail.com>
2024-09-08 17:04:20 +0100

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './globals.css';
+
+function page() {
+  return (
+    <div>
+      <h1>My Website</h1>
+      <p className='title'>Welcome to my website!</p>
+    </div>
+  )
+}
+
+export default page
